018b7afe15a93138e3a7cdc6d462c3c954daa14a
Nguyen Mai Phuong <maiphuong.nguyen.it@gmail.com>
2025-03-06 09:49:45 +0700

    update day3, done header

--- FrontEnd/prime-mart/src/utils/index.js
+++ FrontEnd/prime-mart/src/utils/index.js
@@ -2,5 +2,10 @@ export const ROUTERS = {
     USER: {
         HOME: "",
         PROFILE: "thong-tin-ca-nhan",
+        PRODUCT: "/san-pham",
+        CART: "/gio-hang",
+        CHECKOUT: "/thanh-toan",
+        ORDER: "/don-hang",
+        ORDER_DETAIL: "/chi-tiet-don-hang"
     }
 }
